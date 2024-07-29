9c6a23ab15e6cf72150adbffa3886a6b605158ea
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-07-29 23:51:26 +0300

    move function to actions

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,40 +1,6 @@
-import { redirect } from 'next/navigation';
-import { storePost } from '@/lib/posts';
+import { createPost } from '@/actions/posts';
 import PostForm from '@/components/post-form';
 
-export default async function NewPostPage() {
-  async function createPost(prevState, formData) {
-    'use server';
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let errors = [];
-    if (!title || title.trim().length === 0) {
-      errors.push({ title: 'Title is required!' });
-    }
-
-    if (!image || image.size === 0) {
-      errors.push({ image: 'Image is required!' });
-    }
-
-    if (!content || content.trim().length === 0) {
-      errors.push({ content: 'Content is required!' });
-    }
-
-    if (errors.length > 0) {
-      return {errors};
-    }
-
-    await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect('/feed');
-  }
-
+export default function NewPostPage() {
   return <PostForm action={createPost} />;
 }
