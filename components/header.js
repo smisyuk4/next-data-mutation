e902de5f0690ee8b557059e8cb47ef3c4c50e123
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-07-30 01:23:20 +0300

    fix error with single quote and img tag

--- components/header.js
+++ components/header.js
@@ -1,19 +1,22 @@
-import logo from '@/assets/logo.png';
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '@/assets/logo.png';
 
 export default function Header() {
   return (
-    <header id="main-header">
-      <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+    <header id='main-header'>
+      <Link href='/'>
+        <Image src={logo.src} alt='Mobile phone with posts feed on it' />
       </Link>
       <nav>
         <ul>
           <li>
-            <Link href="/feed">Feed</Link>
+            <Link href='/feed'>Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className='cta-link' href='/new-post'>
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
